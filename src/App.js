f0cd8d67e550d90c3d2c1adb14068dbd9456417a
atanasova <atanas.atanasova@sunrise.net>
2021-09-10 12:56:59 +0200

    added first re-usuable component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 import React from "react";
 import render from "react-dom"
+import Todo from "./component/Todo";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Welcome To React World</h1>
-//     </div>
-//   );
-// }
-
-class App extends React.Component{
-
-    render() {
-        return (
-            <div className="App">
-                <h1>Welcome To React World</h1>
-            </div>
-        );
-    }
-
+function App() {
+    return (
+        <div>
+          <h1>My Todos</h1>
+          <Todo />
+        </div>
+      );
 }
 
 export default App;
